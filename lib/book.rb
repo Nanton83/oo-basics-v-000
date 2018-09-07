62a5fa5d89d46679d8bf7ef2cc0756f20845a274
Nick Romans <nicholas.romans@gmail.com>
2018-09-07 20:37:57 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,6 @@
 
 class Book
-  
+  attr_accessor :author, :page_count, :genre 
 
   
   def initialize(title)
@@ -11,29 +11,29 @@ class Book
     @title
   end 
   
-  def author=(author)
-    @author = author 
-  end 
+#  def author=(author)
+#    @author = author 
+#  end 
   
-  def author 
-    @author 
-  end 
+#  def author 
+#    @author 
+#  end 
   
-  def page_count=(page_count)
-    @page_count = page_count
-  end 
+#  def page_count=(page_count)
+#    @page_count = page_count
+#  end 
   
-  def page_count
-    @page_count
-  end 
+#  def page_count
+#    @page_count
+#  end 
   
-  def genre=(genre)
-    @genre = genre 
-  end 
+#  def genre=(genre)
+#    @genre = genre 
+#  end 
   
-  def genre 
-    @genre 
-  end 
+#  def genre 
+#    @genre 
+#  end 
   
   def turn_page
     puts "Flipping the page...wow, you read fast!"
